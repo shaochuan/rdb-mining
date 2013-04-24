1da16e8ee911697190799e58d7babda9f987da22
Shao-Chuan Wang <shaochuan.wang@gmail.com>
2013-04-24 00:20:51 -0700

    Fix index.py

--- index.py
+++ index.py
@@ -4,14 +4,26 @@ import urllib
 import rethinkdb as r
 from rethinkdb.errors import RqlRuntimeError
 from profile import CandidatePage
-from enum import enum
+from enum import enum, set_last_pid, get_finished, set_finished
 
-def make_index(shard, idx='skills', db, conn):
-    for pid in enum(shard, db, conn):
-        profile = db.table('profile_index').get(pid).run(conn)
+def make_index(conn, shard, idx='skills'):
+    task = 'index'
+    count_for_checkpoint_pid = 20
+
+    if get_finished(conn, task, shard):
+        print 'Task %s at Shard %d finished.' % (task, shard)
+        return
+
+    for loop_count, pid in enumerate(enum(conn, task, shard)):
+        # remember where we are, so we can resume
+        # this pid will be retried when resume
+        if (loop_count+1) % count_for_checkpoint_pid == 0:
+            set_last_pid(conn, task, shard, pid)
+
+        profile = r.table('profile_index').get(pid).run(conn)
         if not profile:
-            db.table('profile_index').insert({'pid':pid, 'indices':[]}).run(conn)
-            profile = db.table('profile_index').get(pid).run(conn)
+            r.table('profile_index').insert({'pid':pid, 'indices':[]}).run(conn)
+            profile = r.table('profile_index').get(pid).run(conn)
         indices = profile['indices']
         if idx in indices:
             # this means this pid has been indexed before
@@ -20,7 +32,7 @@ def make_index(shard, idx='skills', db, conn):
         indices.append(idx)
 
 	try:
-            entry_or_list = db.table('profile').get(pid)[idx].run(conn)
+            entry_or_list = r.table('profile').get(pid)[idx].run(conn)
         except RqlRuntimeError, e:
             continue
         if not entry_or_list:
@@ -29,19 +41,21 @@ def make_index(shard, idx='skills', db, conn):
             entry_or_list = [entry_or_list]
         primary_key = idx[0]+'id'
         for entry in entry_or_list:
-            db.table(idx).insert({primary_key: entry, 'pids':[]}).run(conn)
-            pids = db.table(idx).get(entry)['pids'].run(conn)
+            r.table(idx).insert({primary_key: entry, 'pids':[]}).run(conn)
+            pids = r.table(idx).get(entry)['pids'].run(conn)
             if pid in pids:
                 # this means this pid's index has been processed before
                 continue
-            db.table(idx).get(entry).update({'pids': r.row['pids'].append(pid)}).run(conn)
+            r.table(idx).get(entry).update({'pids': r.row['pids'].append(pid)}).run(conn)
         # tell indexer that we're done
-        db.table('profile_index').update(profile).run(conn)
+        r.table('profile_index').update(profile).run(conn)
+
+    set_finished(conn, task, shard)
+    print 'Task %s at Shard %d finished.' % (task, shard)
 
 if __name__ == '__main__':
     server_ip_dns = sys.argv[1]
-    conn = r.connect(server_ip_dns, port=28015, db='profile')
+    conn = r.connect(server_ip_dns, port=28015, db='people')
     conn.repl()
-    db = r.db('people')
     shard = int(sys.argv[2])
-    make_index(shard)
+    make_index(conn, shard)
