3f72b2e6190ab693172a5f828188b3e4a7398759
Shao-Chuan Wang <shaochuan.wang@gmail.com>
2013-04-26 00:01:45 -0700

    Add duration.py

---
+++ duration.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import sys
+import urllib
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError
+from enum import enum, set_last_pid, get_finished, set_finished
+
+def duration(conn, shard):
+    task = 'duration'
+    for loop_count, pid in enumerate(enum(conn, task, shard)):
+        profile = r.table('profile').get(pid).run(conn)
+        if not profile:
+            print '%s not existed.' % (pid,)
+            continue
+        print profile
+
+        positions = profile.get('positions')
+        if not positions:
+            continue
+        durations = map(lambda x:x.get('duration'), positions)
+        if not any(entry_or_list):
+            continue
+
+        for days in durations:
+            if days >= 10000:
+                continue
+            r.table('duration').get(days).update({'n': r.row['n'] + 1}).run(conn)
+
+if __name__ == '__main__':
+    server_ip_dns = sys.argv[1]
+    conn = r.connect(server_ip_dns, port=28015, db='people')
+    conn.repl()
+    shard = int(sys.argv[2])
+    make_index(conn, shard)
